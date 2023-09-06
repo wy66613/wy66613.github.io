7ea8b4b7960bf0ed3bc286f2b909b22c474c1997
wy66613 <2425332276@qq.com>
2023-09-06 17:52:01 +0800

    update

--- source/js/footer.js
+++ source/js/footer.js
@@ -1,10 +1,8 @@
-document.write('<!-----------  footer----------->');
-document.write('' +
-    '<center>\n' +
-    '<p style="color:#333;">© 2021-2022 TuHome</p>\n' +
-    '</center>\n' +
-    '');
-
-
-
-
+document.write("<!-----------  footer----------->");
+document.write(
+  "" +
+    "<center>\n" +
+    '<p style="color:#333;">© 2021-2023 WyyLib</p>\n' +
+    "</center>\n" +
+    ""
+);
