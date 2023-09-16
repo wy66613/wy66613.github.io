9c1a090eac5ab449a4a00f2a72245bdb4cb90e23
gminisnoob <2425332276@qq.com>
2023-09-17 00:49:35 +0800

    Update footer.js

--- source/js/footer.js
+++ source/js/footer.js
@@ -2,7 +2,7 @@ document.write("<!-----------  footer----------->");
 document.write(
   "" +
     "<center>\n" +
-    '<p style="color:#333;">© 2023 WyyLib  友链:<a href="http://www.xytlib.top">xytlib</a></p>\n' +
+    '<p style="color:#333;">© 2023 WyyLib  友链: <a href="http://www.xytlib.top">xytlib</a></p>\n' +
     "</center>\n" +
     ""
 );
