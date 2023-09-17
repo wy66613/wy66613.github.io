ee3080346fc7a876170122cb7ac06167cf69abe7
wy66613 <2425332276@qq.com>
2023-09-17 13:17:53 +0800

    Update footer.js

--- source/js/footer.js
+++ source/js/footer.js
@@ -2,7 +2,7 @@ document.write("<!-----------  footer----------->");
 document.write(
   "" +
     "<center>\n" +
-    '<p style="color:#333;">© 2023 WyyLib  友链: <a href="http://www.xytlib.top">xytlib</a></p>\n' +
+    '<p style="color:#333;">© 2023 WyyLib  <a href="https://icp.gov.moe/?keyword=20235277" target="_blank">萌ICP备20235277号</a>  <a href="http://www.xytlib.top">友链: xytlib</a></p>\n' +
     "</center>\n" +
     ""
 );
